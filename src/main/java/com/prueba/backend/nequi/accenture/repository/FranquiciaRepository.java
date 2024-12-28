3e9be3514f9d5052c6b93f45d00b90e90fa6790e
FranklinZ12 <franklinq8@gmail.com>
2024-12-28 14:36:48 -0500

    Se crea carpeta repository

--- src/main/java/com/prueba/backend/nequi/accenture/repository/FranquiciaRepository.java
+++ src/main/java/com/prueba/backend/nequi/accenture/repository/FranquiciaRepository.java
@@ -4,6 +4,9 @@ import com.prueba.backend.nequi.accenture.model.entity.Franquicia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FranquiciaRepository extends JpaRepository<Franquicia, Long> {
+    Optional<Franquicia> findByNombre(String nombre);
 }
