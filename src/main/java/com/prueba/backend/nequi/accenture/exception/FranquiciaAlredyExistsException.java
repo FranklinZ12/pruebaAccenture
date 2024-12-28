cb1441803a88b40d719797ca9b1b71d34ca5c2fb
FranklinZ12 <franklinq8@gmail.com>
2024-12-28 13:19:29 -0500

    Se agrega carpeta de exceptions

---
+++ src/main/java/com/prueba/backend/nequi/accenture/exception/FranquiciaAlredyExistsException.java
@@ -0,0 +1,8 @@
+package com.prueba.backend.nequi.accenture.exception;
+
+public class FranquiciaAlredyExistsException extends RuntimeException{
+
+    public FranquiciaAlredyExistsException(String message) {
+        super(message);
+    }
+}
