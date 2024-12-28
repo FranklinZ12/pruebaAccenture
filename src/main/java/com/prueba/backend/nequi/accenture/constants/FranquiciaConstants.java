a1cfde5e7ea21cb3e68fa6366eee5de90a638644
FranklinZ12 <franklinq8@gmail.com>
2024-12-28 14:33:48 -0500

    Se agrega carpeta de controller y constans

---
+++ src/main/java/com/prueba/backend/nequi/accenture/constants/FranquiciaConstants.java
@@ -0,0 +1,6 @@
+package com.prueba.backend.nequi.accenture.constants;
+
+public class FranquiciaConstants {
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Franquicia creada con exito";
+}
---
+++ src/main/java/com/prueba/backend/nequi/accenture/controller/FranquiciaController.java
@@ -0,0 +1,30 @@
+package com.prueba.backend.nequi.accenture.controller;
+
+import com.prueba.backend.nequi.accenture.constants.FranquiciaConstants;
+import com.prueba.backend.nequi.accenture.model.dto.FranquiciaDto;
+import com.prueba.backend.nequi.accenture.model.dto.ResponseDto;
+import com.prueba.backend.nequi.accenture.services.IFranquiciaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+public class FranquiciaController {
+
+    private IFranquiciaService iFranquiciaService;
+
+    @PostMapping("/crearFranquicia")
+    public ResponseEntity<ResponseDto> crearFranquicia(@RequestBody FranquiciaDto franquiciaDto){
+        iFranquiciaService.creaFranquicia(franquiciaDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(FranquiciaConstants.STATUS_201, FranquiciaConstants.MESSAGE_201));
+    }
+}
